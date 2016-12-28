2bc270683ed3fd79b002e711af06bd36ffaef003
Jared Hanson <jaredhanson@gmail.com>
2016-12-28 15:56:23 -0800

    Add test cases.

--- test/exchange/oob.test.js
+++ test/exchange/oob.test.js
@@ -8,4 +8,16 @@ describe('exchange.oob', function() {
     expect(oob(function(){}, function(){}).name).to.equal('oob');
   });
   
+  it('should throw if constructed without an authenticate callback', function() {
+    expect(function() {
+      oob();
+    }).to.throw(TypeError, 'oauth2orize-2fa.oob exchange requires an authenticate callback');
+  });
+  
+  it('should throw if constructed without an issue callback', function() {
+    expect(function() {
+      oob(function(){});
+    }).to.throw(TypeError, 'oauth2orize-2fa.oob exchange requires an issue callback');
+  });
+  
 });
