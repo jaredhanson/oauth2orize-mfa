8bcccc5fc6c5d943388c22361c3dcd4749fa8f15
Jared Hanson <jaredhanson@gmail.com>
2017-01-02 14:06:16 -0800

    Make test case.

--- test/exchange/oob.test.js
+++ test/exchange/oob.test.js
@@ -61,4 +61,47 @@ describe('exchange.oob', function() {
     });
   });
   
+  describe('authenticating and issuing an access token based on scope', function() {
+    var response, err;
+
+    before(function(done) {
+      function authenticate(token, done) {
+        if (token !== 'ey...') { return done(new Error('incorrect token argument')); }
+        
+        return done(null, { id: '1', username: 'johndoe' })
+      }
+      
+      function issue(client, user, oobCode, scope, done) {
+        if (client.id !== 'c123') { return done(new Error('incorrect client argument')); }
+        if (user.username !== 'johndoe') { return done(new Error('incorrect user argument')); }
+        if (oobCode !== 'a1b2c3') { return done(new Error('incorrect oobCode argument')); }
+        if (scope.length !== 1) { return done(new Error('incorrect scope argument')); }
+        if (scope[0] !== 'execute') { return done(new Error('incorrect scope argument')); }
+        
+        return done(null, 's3cr1t')
+      }
+      
+      chai.connect.use(oob(authenticate, issue))
+        .req(function(req) {
+          req.user = { id: 'c123', name: 'Example' };
+          req.body = { access_token: 'ey...', oob_code: 'a1b2c3', scope: 'execute' };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"s3cr1t","token_type":"Bearer"}');
+    });
+  });
+  
 });
